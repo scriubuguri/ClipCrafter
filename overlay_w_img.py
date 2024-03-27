6334765a0c58d8c4967dae94e6c4187e02f53f0d
scriubuguri <scriubuguri@tragdate.ninja>
2024-03-27 21:12:09 +0000

    few updates

--- overlay_w_img.py
+++ overlay_w_img.py
@@ -20,7 +20,7 @@ timestamps = [x.strip() for x in timestamps if x.strip()]
 for wordnumber, timestamp in enumerate(timestamps, start=1):
     tsstart, tsstop = timestamp.split(" ")
 
-    command = f"""ffmpeg -i {input_video}{wordnumber}.mp4 -i word_images/word_{wordnumber}.png -filter_complex "[0:v][1:v] overlay=25:25:enable='between(t, {tsstart}, {tsstop})'" -pix_fmt yuv420p -c:a copy {input_video}{wordnumber+1}.mp4"""
+    command = f"""ffmpeg -i {input_video}{wordnumber}.mp4 -i word_images/word_{wordnumber}.png -filter_complex "[0:v][1:v] overlay=(W-w)/2:H-h-25:enable='between(t, {tsstart}, {tsstop})'" -pix_fmt yuv420p -c:a copy {input_video}{wordnumber+1}.mp4"""
 
     print(command)
     call(command, shell=True)
